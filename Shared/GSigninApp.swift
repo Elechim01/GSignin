7bc707f54a9d3cb556f6ee93bc4be75027233f67
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-14 22:13:47 +0100

    Initial Commit

---
+++ Shared/GSigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSigninApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GSigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
